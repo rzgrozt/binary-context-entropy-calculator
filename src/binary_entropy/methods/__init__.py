3afb61022754f1e062154b185e0204103041e23f
RzgrOzt <32547215+rzgrozt@users.noreply.github.com>
2026-09-02 13:11:09 +0200

    Expose VMM method functions

--- src/binary_entropy/methods/__init__.py
+++ src/binary_entropy/methods/__init__.py
@@ -8,12 +8,16 @@ from binary_entropy.methods.markov import (
     predict_markov,
 )
 from binary_entropy.methods.shannon import analyze_shannon
+from binary_entropy.methods.vmm import analyze_vmm, analyze_vmm_per_sequence, fit_vmm
 
 __all__ = [
     "analyze_hmm",
     "analyze_markov",
     "analyze_markov_per_sequence",
     "analyze_shannon",
+    "analyze_vmm",
+    "analyze_vmm_per_sequence",
     "fit_markov",
+    "fit_vmm",
     "predict_markov",
 ]
